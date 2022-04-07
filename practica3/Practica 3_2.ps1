1e42022642c992260b0f6145e48a2414b7fc223d
Naughtar <68660632+Naughtar@users.noreply.github.com>
2022-04-06 23:00:05 -0500

    Add files via upload

---
+++ practica3/Practica 3_2.ps1
@@ -0,0 +1,11 @@
+﻿function leer{
+    Param([string]$a)
+    [string] $x = Get-Date -Format " dd/MM/yyyy HH:mm"
+    Write-Output ("Hola $a, la hora es: ", $x)
+    $p =Get-Process explorer | Format-List Id
+    Write-Output("El proceso explorer tiene la ID: ", $p,)
+
+
+}
+[string] $nombre = Read-Host "Inserte su nombre" 
+leer $nombre 
